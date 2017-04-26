9ef8958d82ed51a918b0c2ff68c6fb1334948d71
duantianhen2014 <duantianhen2014@gmail.com>
2017-04-26 19:00:03 +0800

    Create 文件类型检测.php
    读取文件两个字节的方式

---
+++ 文件类型检测.php
@@ -0,0 +1,48 @@
+<?php
+/** 
+* 读取文件前几个字节 判断文件类型 
+* @return String 
+*/ 
+function checkTitle($filename){ 
+  $file=fopen($filename, "rb"); 
+  $bin=fread($file, 2); //只读2字节 
+  fclose($file); 
+  $strInfo =@unpack("c2chars", $bin); 
+  $typeCode=intval($strInfo['chars1'].$strInfo['chars2']); 
+  $fileType=''; 
+  switch($typeCode){ 
+    case 7790: 
+      $fileType='exe'; 
+    break; 
+    case 7784: 
+      $fileType='midi'; 
+    break; 
+    case 8297: 
+      $fileType='rar'; 
+    break; 
+    case 255216: 
+      $fileType='jpg'; 
+    break; 
+    case 7173: 
+      $fileType='gif'; 
+    break; 
+    case 6677: 
+      $fileType='bmp'; 
+    break; 
+    case 13780: 
+      $fileType='png'; 
+    break; 
+    default: 
+      $fileType='unknown'.$typeCode; 
+    break; 
+  } 
+  //Fix 
+  if($strInfo['chars1']=='-1' && $strInfo['chars2']=='-40'){ 
+    return 'jpg'; 
+  } 
+  if($strInfo['chars1']=='-119' && $strInfo['chars2']=='80'){ 
+    return 'png'; 
+  } 
+  return $fileType; 
+} 
+echo checkTitle('0.jpg');
